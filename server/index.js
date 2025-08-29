75766b45c57ad19d4bce65209728f20c3ecafcf4
harisinghbisht <h.bisht@satneel.com>
2025-08-30 01:21:49 +0530

    added endpoint git push

--- server/index.js
+++ server/index.js
@@ -12,11 +12,12 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: true,
-    credentials: true,
+    origin: "*", 
+    credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
